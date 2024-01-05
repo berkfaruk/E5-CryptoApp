9c8cdef8024e63423b68d93f625b100c8c37a0ff
Berk Faruk Çınar <berkfaruk97@gmail.com>
2024-01-05 17:12:17 +0300

    Project Upload

---
+++ app/src/main/java/com/bfc/retrofitkotlin/service/CryptoAPI.kt
@@ -0,0 +1,22 @@
+package com.bfc.retrofitkotlin.service
+
+import com.bfc.retrofitkotlin.model.CryptoModel
+import io.reactivex.Observable
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface CryptoAPI {
+
+    //GET, POST, UPDATE, DELETE
+
+    //https://raw.githubusercontent.com/
+    // atilsamancioglu/K21-JSONDataSet/master/crypto.json
+
+    @GET("atilsamancioglu/K21-JSONDataSet/master/crypto.json")
+
+    fun getData() : Observable<List<CryptoModel>>
+
+    //fun getData() : Call<List<CryptoModel>>
+
+
+}
